b6d815cdfd75757778a4522bba414d415560692a
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-07-03 14:58:16 +0530

    Create variable.tf

---
+++ Azure/SQL/variable.tf
@@ -0,0 +1,47 @@
+variable "resource_group_name" {
+  description = "Default resource group name that the database will be created in."
+  default     = "yash-rg"
+}
+
+variable "location" {
+  description = "The location/region where the database and server are created."
+  default     = "East US"
+}
+
+variable "db_name" {
+  description = "The name of the database to be created."
+  default     = "yash-db"
+}
+
+variable "sql_admin_username" {
+  description = "The administrator username of the SQL Server."
+  default     = "tfsquad"
+}
+
+
+variable "sql_password" {
+  description = "The administrator password of the SQL Server."
+  default     = "yash@1234567"
+}
+
+variable "server_version" {
+  description = "The version for the database server. Valid values are: 2.0 (for v11 server) and 12.0 (for v12 server)."
+  default     = "12.0"
+}
+
+
+variable "db_edition" {
+  description = "The edition of the database to be created."
+  default     = "Basic"
+}
+
+variable "service_objective_name" {
+  description = "The performance level for the database. For the list of acceptable values, see https://docs.microsoft.com/en-gb/azure/sql-database/sql-database-service-tiers. Default is Basic."
+  default     = "Basic"
+}
+
+variable "collation" {
+  description = "The collation for the database. Default is SQL_Latin1_General_CP1_CI_AS"
+  default     = "SQL_Latin1_General_CP1_CI_AS"
+}
+
