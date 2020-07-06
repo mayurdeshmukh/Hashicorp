da5057b1e40a92c9a60ee7169adf97023a2ab6e9
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-07-06 15:58:19 +0530

    Create provider.tf

---
+++ Azure/ACI Devops Agent/provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "5f3351a3-fe"
+  client_id       = "7cb6aa76-617b060ff"
+  client_secret   = "9tOXuRBxF-.1go32Iad"
+  tenant_id       = "4bbba08c-85"
+
+  features {}
+}
