890beeb58782a253ab9fba67d31dedbb72a3a5b6
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-07-06 16:44:44 +0530

    Update main.tf

--- Azure/ACI Devops Agent/main.tf
+++ Azure/ACI Devops Agent/main.tf
@@ -7,26 +7,7 @@ module "aci-devops-agent" {
   enable_vnet_integration = false
   create_resource_group   = true
 
-  //linux_agents_configuration = {
-  //  agent_name_prefix = var.agent_name_prefix
-  //  agent_pool_name   = var.agent_pool_name
-  //  count             = var.count
-  //  docker_image      = var.docker_image
-  //  docker_tag        = var.docker_tag
-  //  cpu               = var.cpu
-  //  memory            = var.memory
-  //}
-  linux_agents_configuration = {
-    agent_name_prefix = "linux-agent"
-    agent_pool_name   = "Azure Pipelines"
-    count             = 2,
-    docker_image      = "jcorioland/aci-devops-agent"
-    docker_tag        = "0.2-linux"
-    cpu               = 1
-    memory            = 4
-  }
-
-
+  linux_agents_configuration = var.linux_agents_configuration
   azure_devops_org_name              = var.azure_devops_org_name
   azure_devops_personal_access_token = var.azure_devops_personal_access_token
 }
