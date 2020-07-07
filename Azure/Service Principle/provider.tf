3d0ec1e20edbccad57e721baf7a7be86daf1e34f
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-07-07 17:37:04 +0530

    Create provider.tf

---
+++ Azure/Service Principle/provider.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  
+  subscription_id = "5f3351a3-a9f4-b09a43bd872e"
+  client_id       = "7cb6aa764a2b060ff"
+  client_secret   = "9tOXuRBxad"
+  tenant_id       = "4bbba08c05"
+
+  features {}
+
+}
