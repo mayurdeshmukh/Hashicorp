8b2ed9cddb33520eab7d8eaec40fbd77d37d5fed
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-07-08 15:02:07 +0530

    Create provider.tf

---
+++ Azure/Load Balancer/provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "5f3351a3-f172-"
+  client_id       = "7cb6aa76-6176-f"
+  client_secret   = "9tOXuRBxF-.1gd"
+  tenant_id       = "4bbba08c-896d05"
+
+  features {}
+}
