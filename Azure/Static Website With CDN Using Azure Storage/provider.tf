119f07c8dcdc585efb4a8ad966e349673feecf7f
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-07-09 16:04:40 +0530

    Create provider.tf

---
+++ Azure/Static Website With CDN Using Azure Storage/provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "5f3351a3-f1d872e"
+  client_id       = "7cb6aa76-6176ff"
+  client_secret   = "9tOXuRBxF-.1go2Iad"
+  tenant_id       = "4bbba08c-86d05"
+
+  features {}
+}
