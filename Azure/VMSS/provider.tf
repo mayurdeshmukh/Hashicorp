4a02ef9a7518eec88558f3ed5f2bf3a719abe682
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-07-09 13:18:26 +0530

    Create provider.tf

---
+++ Azure/VMSS/provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "5f3351a3-f1"
+  client_id       = "7cb6aa76-617b060ff"
+  client_secret   = "9tOXuRBxF-.1ad"
+  tenant_id       = "4bbba08c-05"
+
+  features {}
+}
